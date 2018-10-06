8ad7d57bd4571d6bb8cf57c76c32eefeb9d1ce16
Shadow63 <peterlee26717@yahoo.com>
2018-10-06 15:37:25 -0400

    HayStack.py test code

---
+++ APIStuff/HayStack.py
@@ -0,0 +1,6 @@
+import requests
+
+r = requests.post(
+  "https://api.haystack.ai/api/image/analyze?output=json&apikey=3401986c5465b90cfdb5021ff1d0f27e",
+  data=open('testImage.jpg', 'rb'))
+print(r.text);
