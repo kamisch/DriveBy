02fa7481e00e7eca1fd92886a7a0281732d0397b
tyenss <yuuki.butler@gmail.com>
2018-10-06 18:33:48 -0400

    added stuff and nuget

---
+++ DriveBy/Location/GeoLocationMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DriveBy
+{
+    public class GeoLocationMap
+    {
+        public class LocationUpdatedEventArgs : EventArgs
+{
+    CLLocation location;
+
+    public LocationUpdatedEventArgs(CLLocation location)
+    {
+       this.location = location;
+    }
+
+    public CLLocation Location
+    {
+       get { return location; }
+    }
+}
+    }
+}
