2b584e6dd3c442f214abad0897cbb558ca3f2978
tyenss <yuuki.butler@gmail.com>
2018-10-06 21:44:39 -0400

    asdf

---
+++ DriveBy/MapThings/MapData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DriveBy.MapStuff
+{
+    public class MapData
+    {
+        public double Latitude {get;set;}
+        public double Longitude {get;set;}
+        public string RestaurantType{get;set;}
+        public string RestaurantName{get;set;}
+        
+        public MapData(double latitude,double longitude,string restaurantName,string restaurantType)
+        {
+            Latitude=latitude;
+            Longitude=longitude;
+            RestaurantType=restaurantType;
+            RestaurantName=restaurantName;
+        }
+    }
+}
---
+++ DriveBy/MapThings/MapStuff.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms.Maps;
+
+
+namespace DriveBy
+{
+    public class MapStuff
+    {
+        public MapStuff()
+        {
+            
+        }
+        
+        public void LocationToPin()
+        {
+        
+        }
+        
+        public void LocationToPinArray()
+        {
+            Pin pin = new Pin (){
+            Position = new Position (37, -122),
+            Label = "Some Pin!"
+            };
+            
+            
+        }
+        
+        public static void AddressToLatLong()
+        {
+            var address = "Stavanger, Norway";
+
+            var locationService = new GoogleLocationService();
+            var point = locationService.GetLatLongFromAddress(address);
+        
+            var latitude = point.Latitude;
+            var longitude = point.Longitude;
+        }
+    }
+}
