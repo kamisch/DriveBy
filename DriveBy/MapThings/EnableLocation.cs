7c709ee5ee255bf3169b1399764b1fe25ca24a49
Shadow63 <peterlee26717@yahoo.com>
2018-10-06 23:24:04 -0400

    added enable locations

--- DriveBy/MapThings/EnableLocation.cs
+++ DriveBy/MapThings/EnableLocation.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using Xamarin.Essentials;
 using Plugin.Geolocator;
+using Xamarin.Forms.Map;
 
-namespace DriveBy.MapThings
+namespace DriveBy.MapStuff
 {
     public class EnableLocation
     {
-        public EnableLocation()
+        public EnableLocation(Map map)
         {
+            CLLocationManager locationManager = new CLLocationManager();
+            locationManager.RequestWhenInUseAuthorization();
+            map.ShowsUserLocation = true;
             
+            // add an annotation
+            map.AddAnnotations (new MKPointAnnotation (){
+                Title="MyAnnotation",
+                Coordinate = new CLLocationCoordinate2D (42.364260, -71.120824)
+            });
         }
     }
+}
 }
